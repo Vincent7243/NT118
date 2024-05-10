d1cf0ec806799bc136a1b1e6d8610fd5ba37143c
Vincent <22520130@gm.uit.edu.vn>
2024-05-11 00:50:42 +0700

    done bottom navigation bar java

--- app/src/main/java/com/company/soccershoesstore/BottomNavigationBar.java
+++ app/src/main/java/com/company/soccershoesstore/BottomNavigationBar.java
@@ -1,8 +1,14 @@
 package com.company.soccershoesstore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class BottomNavigationBar extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class BottomNavigationBar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation_bar);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation_bar);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_cozy) {
+                    item.setIcon(R.drawable.ic_cozy_fill);
+                    Toast.makeText(BottomNavigationBar.this, "Categories", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.action_love) {
+                    item.setIcon(R.drawable.ic_love_fill);
+                    Toast.makeText(BottomNavigationBar.this, "Favorites", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.action_home) {
+                    item.setIcon(R.drawable.ic_home_fill);
+                    Toast.makeText(BottomNavigationBar.this, "Home", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.action_notification) {
+                    item.setIcon(R.drawable.ic_notification_fill);
+                    Toast.makeText(BottomNavigationBar.this, "Notification", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.action_person) {
+                    item.setIcon(R.drawable.ic_person_fill);
+                    Toast.makeText(BottomNavigationBar.this, "Person", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
     }
 }
