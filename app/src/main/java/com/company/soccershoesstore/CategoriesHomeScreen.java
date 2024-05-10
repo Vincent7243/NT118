97cf8201a107bb5e1a896c59ee77075e261b2b74
Vincent <22520130@gm.uit.edu.vn>
2024-05-10 08:56:17 +0700

    add categories

---
+++ app/src/main/java/com/company/soccershoesstore/CategoriesHomeScreen.java
@@ -0,0 +1,14 @@
+package com.company.soccershoesstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoriesHomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories_home_screen);
+    }
+}
