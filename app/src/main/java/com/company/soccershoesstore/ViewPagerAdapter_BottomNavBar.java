a4bad8f466dd5bccc11aa14c5451735dfd686956
Vincent <22520130@gm.uit.edu.vn>
2024-05-11 19:24:50 +0700

    update Bottom Navigation Bar

--- app/src/main/java/com/company/soccershoesstore/ViewPagerAdapter_BottomNavBar.java
+++ app/src/main/java/com/company/soccershoesstore/ViewPagerAdapter_BottomNavBar.java
@@ -6,8 +6,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class ViewPagerAdaper_BottomNavBar extends FragmentStateAdapter {
-    public ViewPagerAdaper_BottomNavBar(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+public class ViewPagerAdapter_BottomNavBar extends FragmentStateAdapter {
+    public ViewPagerAdapter_BottomNavBar(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
         super(fragmentManager, lifecycle);
     }
 
@@ -16,17 +16,9 @@ public class ViewPagerAdaper_BottomNavBar extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position){
             case 0:
-                return new Fragment();
-            case 1:
-                return new Fragment();
-            case 2:
-                return new Fragment();
-            case 3:
-                return new Fragment();
-            case 4:
-                return new Fragment();
+                return new HomeFragment();
             default:
-                return new Fragment();
+                return new HomeFragment();
         }
 
     }
