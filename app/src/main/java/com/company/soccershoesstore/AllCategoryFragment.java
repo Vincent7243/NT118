d316566b873c84228028d08330725fb87dfedb02
Vincent <22520130@gm.uit.edu.vn>
2024-05-20 21:22:12 +0700

    add backpress on all category fragment and change code in category fragment

---
+++ app/src/main/java/com/company/soccershoesstore/AllCategoryFragment.java
@@ -0,0 +1,31 @@
+package com.company.soccershoesstore;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AllCategoryFragment extends Fragment {
+    public Button BackPress;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_all_category, container, false);
+        BackPress = view.findViewById(R.id.back_button);
+
+        BackPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getParentFragmentManager().popBackStack();
+            }
+        });
+
+        return view;
+    }
+}
