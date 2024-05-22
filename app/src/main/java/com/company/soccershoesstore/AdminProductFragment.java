03b10a3364d83cfd42a2f93f3b1c9275e9855014
TrongNhan73 <trongnhan0703it@gmail.com>
2024-05-22 15:08:51 +0700

    get product from firebase and show in product tab

--- app/src/main/java/com/company/soccershoesstore/AdminProductFragment.java
+++ app/src/main/java/com/company/soccershoesstore/AdminProductFragment.java
@@ -1,18 +1,32 @@
 package com.company.soccershoesstore;
 
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.ktx.Firebase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -22,21 +36,40 @@ public class AdminProductFragment extends Fragment {
     RecyclerView rv;
     ArrayList<Product> products;
     ProductListAdapter adapter;
+    FirebaseFirestore db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_admin_product, container, false);
         ib_add=view.findViewById(R.id.ib_admin_product_add);
+
         rv=view.findViewById(R.id.rv_admin_product);
+        db=FirebaseFirestore.getInstance();
         products=new ArrayList<>();
-        products.add(new Product("test","San pham 1","","","",""));
-        products.add(new Product("test","San pham 1","","","",""));
-        products.add(new Product("test","San pham 1","","","",""));
-        products.add(new Product("test","San pham 1","","","",""));
-        products.add(new Product("test","San pham 1","","","",""));
-    adapter=new ProductListAdapter(view.getContext(),products);
-    rv.setAdapter(adapter);
-        rv.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+
+        
+        db.collection("Products")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("firebasefirestore", document.getId() + " => " + document.getData());
+                                Log.d("firebasefirestore",  document.getData().get("name").toString());
+                                products.add(new Product(document.getId().toString(),document.getData().get("name").toString(),document.getData().get("price").toString(),document.getData().get("image").toString(),document.getData().get("description").toString(),document.getData().get("brand").toString()));
+                                adapter=new ProductListAdapter(view.getContext(),products);
+                                rv.setAdapter(adapter);
+                                rv.setLayoutManager(new LinearLayoutManager(view.getContext()));
+                            }
+                        }else {
+                            Log.w("firebasefirestore", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
 
         ib_add.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/company/soccershoesstore/ProductListAdapter.java
+++ app/src/main/java/com/company/soccershoesstore/ProductListAdapter.java
@@ -1,16 +1,27 @@
 package com.company.soccershoesstore;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.Image;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +48,54 @@ private ArrayList<Product> mproducts;
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Product product=mproducts.get(position);
+        holder.tv_name.setText(product.getMname());
+        holder.tv_brand.setText(product.getMbrand());
+        holder.tv_price.setText(product.getMprice()+"vnd");
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://nt118-6829d.appspot.com");
+        StorageReference storageRef = storage.getReferenceFromUrl(product.getMimage());
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String res= uri.toString();
+                Glide.with(mcontext)
+                        .load(res)
+                        .into(holder.iv);
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+               Toast.makeText(mcontext,e.toString(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        holder.ib_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mcontext,""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.ib_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(mcontext)
+                        .setTitle("Delete confirm")
+                        .setMessage("Are you sure you want to delete this product?")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Continue with delete operation
+                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(R.drawable.warning)
+                        .show();
+            }
+        });
     }
 
     @Override
@@ -55,6 +113,8 @@ ImageButton ib_delete,ib_edit;
         tv_name=itemView.findViewById(R.id.tv_admin_product_item_name);
         tv_brand=itemView.findViewById(R.id.tv_admin_product_item_brand);
         tv_price=itemView.findViewById(R.id.tv_admin_product_item_price);
+        ib_delete=itemView.findViewById(R.id.ib_admin_product_item_delete);
+        ib_edit=itemView.findViewById(R.id.ib_admin_product_item_edit);
     }
 }
 }
