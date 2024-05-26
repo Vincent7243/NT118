128d291b34b9bfa350c2d3d13dd207bea2455673
nguyentiendatntd2003 <nguyentiendatntd2003@gmail.com>
2024-05-26 15:16:45 +0700

    fix

--- app/src/main/java/com/company/soccershoesstore/AccountFragment.java
+++ app/src/main/java/com/company/soccershoesstore/AccountFragment.java
@@ -1,10 +1,12 @@
 package com.company.soccershoesstore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +15,22 @@ import androidx.fragment.app.Fragment;
 
 
 public class AccountFragment extends Fragment {
+    ImageButton imageButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account,container,false);
+        goToActivity();
         return view;
     }
-
-
-
-
+    public void goToActivity() {
+        imageButton = imageButton.findViewById(R.id.img_doiten);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Change_info.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
