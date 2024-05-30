9032c91cb3b069c0a1601ac696ac422ad6ff5e9a
TrongNhan73 <trongnhan0703it@gmail.com>
2024-05-30 20:39:36 +0700

    complete manage sale admin

--- app/src/main/java/com/company/soccershoesstore/Saleadapter.java
+++ app/src/main/java/com/company/soccershoesstore/Saleadapter.java
@@ -2,6 +2,7 @@ package com.company.soccershoesstore;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,11 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
@@ -45,13 +51,35 @@ public class Saleadapter extends ArrayAdapter<Sale> {
         ib_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mcontext, "edit"+position, Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(mcontext.getApplicationContext(), activity_admin_sale_edit.class);
+                intent.putExtra("iid",sale.getMid());
+                intent.putExtra("mcode",sale.getMcode());
+                intent.putExtra("mprice",sale.getMprice());
+                intent.putExtra("mquantity",sale.getMquantity());
+                mcontext.startActivity(intent);
             }
         });
         ib_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mcontext, "delete"+position, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mcontext, "delete"+position, Toast.LENGTH_SHORT).show();
+                new AlertDialog.Builder(mcontext)
+                        .setTitle("Delete confirm")
+                        .setMessage("Are you sure you want to delete this Sale?")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                deleteSale(sale.getMid());
+
+                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(R.drawable.warning)
+                        .show();
 
             }
         });
@@ -67,5 +95,39 @@ public class Saleadapter extends ArrayAdapter<Sale> {
             }
         });
         return convertView;
+    }
+    public interface OnSaleDeleteListener {
+        void onSaleDeleted();
+    }
+
+    private OnSaleDeleteListener mListener;
+
+    public void setOnSaleDeleteListener(OnSaleDeleteListener listener) {
+        this.mListener = listener;
+    }
+    public void deleteSale(String iid) {
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("sales").document(iid)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("delete sale", "DocumentSnapshot successfully deleted!");
+
+
+                        if (mListener != null) {
+                            mListener.onSaleDeleted();}
+
+
+                        Toast.makeText(mcontext, "delete sucessful", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("delete sale", "Error deleting document", e);
+                        Toast.makeText(mcontext, "delete failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
