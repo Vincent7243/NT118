1b6f06cd2d2acd0b3b38f02681da8ef255ae6dd0
nguyentiendatntd2003 <nguyentiendatntd2003@gmail.com>
2024-06-13 11:10:59 +0700

    edit activity_order_status.xml, add id_product in BillItem.java

--- app/src/main/java/com/company/soccershoesstore/BillItem.java
+++ app/src/main/java/com/company/soccershoesstore/BillItem.java
@@ -4,13 +4,35 @@ public class BillItem {
     private String id_bill;
     private String quan;
     private String total;
+    private String id_product;
 
     public BillItem() {}
 
-    public BillItem(String id_bill, String quan, String total) {
+    public BillItem(String id_bill, String quan, String total,String id_product) {
         this.id_bill = id_bill;
         this.quan = quan;
         this.total = total;
+        this.id_product = id_product;
+    }
+
+    public void setId_bill(String id_bill) {
+        this.id_bill = id_bill;
+    }
+
+    public void setQuan(String quan) {
+        this.quan = quan;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public void setId_product(String id_product) {
+        this.id_product = id_product;
+    }
+
+    public String getId_product() {
+        return id_product;
     }
 
     public String getId_bill() {
